3353901f3578e1c89bc97e2e9df1207386ce14f1
Rafael Magalhaes <rafaelmagalhaes93@gmail.com>
2017-08-07 11:01:36 +0100

    show loading bar when searching

--- app/scripts/controllers/finduser.js
+++ app/scripts/controllers/finduser.js
@@ -1,32 +1,35 @@
 'use strict';
 
 app.controller('FindUser', function ($scope, GithubApi) {
+  $scope.userName = '';
+
+  $scope.userNotFound = false;
+  $scope.loaded = false;
+  $scope.loading = false;
+  $scope.Search = function () {
+    $scope.loading = true;
+
+    GithubApi.getUser($scope.userName).then(function (data) {
+      $scope.user = data.data;
+      $scope.loaded = true;
+      $scope.userNotFound = false;
+      $scope.loading = false;
+
+    }).catch(function () {
+      $scope.userNotFound = true;
+      $scope.loaded = false;
+    });
+    GithubApi.getUserRepos($scope.userName).then(function (data) {
+      $scope.repos = data.data;
+      $scope.reposFound = $scope.repos.length > 0;
+    });
+  };
+
+
+  $scope.clearUser = function () {
     $scope.userName = '';
-
-    $scope.userNotFound = false;
     $scope.loaded = false;
-
-    $scope.Search = function () {
-        GithubApi.getUser($scope.userName).then(function (data) {
-            $scope.user = data.data;
-            $scope.loaded = true;
-            $scope.userNotFound = false;
-
-        }).catch(function () {
-            $scope.userNotFound = true;
-            $scope.loaded = false;
-        });
-        GithubApi.getUserRepos($scope.userName).then(function (data) {
-            $scope.repos = data.data;
-            $scope.reposFound = $scope.repos.length > 0;
-        });
-    };
-
-
-    $scope.clearUser = function () {
-        $scope.userName = '';
-        $scope.loaded = false;
-        $scope.userNotFound = false;
-    };
+    $scope.userNotFound = false;
+  };
 
 });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,30 +3,33 @@
 
 app.controller('MainCtrl', function ($scope, GithubApi) {
 
-    $scope.repoNotFound = false;
-    $scope.loaded = false;
-    $scope.repository ='';
-    $scope.Search = function () {
-        GithubApi.getRepos($scope.repository)
-            .then(function (data) {
-                $scope.loaded = true;
-                $scope.repo = data.data;
-                $scope.repoNotFound = false;
+  $scope.repoNotFound = false;
+  $scope.loaded = false;
+  $scope.loading = false;
+  $scope.repository = '';
 
-                if ($scope.repo.total_count === 0) {
-                    $scope.repoNotFound = true;
-                    $scope.loaded = false;
-                }
-            })
-            .catch(function () {
-                $scope.repoNotFound = true;
-                $scope.loaded = false;
-
-            });
-    };
-    $scope.clearUser = function () {
-        $scope.repository = '';
-        $scope.loaded = false;
+  $scope.Search = function () {
+    $scope.loading = true;
+    $scope.httpPromise = GithubApi.getRepos($scope.repository)
+      .then(function (data) {
+        $scope.loaded = true;
+        $scope.repo = data.data;
         $scope.repoNotFound = false;
-    };
+        $scope.loading = false;
+        if ($scope.repo.total_count === 0) {
+          $scope.repoNotFound = true;
+          $scope.loaded = false;
+        }
+      })
+      .catch(function () {
+        $scope.repoNotFound = true;
+        $scope.loaded = false;
+
+      });
+  };
+  $scope.clearUser = function () {
+    $scope.repository = '';
+    $scope.loaded = false;
+    $scope.repoNotFound = false;
+  };
 });
